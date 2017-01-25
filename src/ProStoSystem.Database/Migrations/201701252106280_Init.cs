4e24c329297485158ff48c9eced75eb8473eecc1
Lukasz Rabiec <lukaszrabiec@gmail.com>
2017-01-25 22:14:37 +0100

    Added Init migration and Seed methods

---
+++ src/ProStoSystem.Database/Migrations/201701252106280_Init.cs
@@ -0,0 +1,188 @@
+namespace ProStoSystem.Database.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    public partial class Init : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.BillTypes",
+                c => new
+                {
+                    Id = c.Int(nullable: false, identity: true),
+                    Name = c.String(nullable: false),
+                })
+                .PrimaryKey(t => t.Id);
+
+            CreateTable(
+                "dbo.OrderDetails",
+                c => new
+                {
+                    Id = c.Int(nullable: false, identity: true),
+                    Customer = c.String(nullable: false),
+                    OrderDate = c.DateTimeOffset(nullable: false, precision: 7),
+                    Comment = c.String(),
+                    Discount = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    BillTypeId = c.Int(nullable: false),
+                    SalesmanId = c.Int(nullable: false),
+                })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.BillTypes", t => t.BillTypeId, cascadeDelete: true)
+                .ForeignKey("dbo.Salesmen", t => t.SalesmanId, cascadeDelete: true)
+                .Index(t => t.BillTypeId)
+                .Index(t => t.SalesmanId);
+
+            CreateTable(
+                "dbo.Products",
+                c => new
+                {
+                    Id = c.Int(nullable: false, identity: true),
+                    Name = c.Int(nullable: false),
+                    SellingPrice = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    PurchasePrice = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    Amount = c.Int(nullable: false),
+                    CategoryId = c.Int(nullable: false),
+                })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Categories", t => t.CategoryId, cascadeDelete: true)
+                .Index(t => t.CategoryId);
+
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                {
+                    Id = c.Int(nullable: false, identity: true),
+                    Name = c.String(nullable: false),
+                })
+                .PrimaryKey(t => t.Id);
+
+            CreateTable(
+                "dbo.Salesmen",
+                c => new
+                {
+                    Id = c.Int(nullable: false, identity: true),
+                    FirstName = c.String(nullable: false),
+                    LastName = c.String(nullable: false),
+                })
+                .PrimaryKey(t => t.Id);
+
+            CreateTable(
+                "dbo.AspNetRoles",
+                c => new
+                {
+                    Id = c.String(nullable: false, maxLength: 128),
+                    Name = c.String(nullable: false, maxLength: 256),
+                })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.Name, unique: true, name: "RoleNameIndex");
+
+            CreateTable(
+                "dbo.AspNetUserRoles",
+                c => new
+                {
+                    UserId = c.String(nullable: false, maxLength: 128),
+                    RoleId = c.String(nullable: false, maxLength: 128),
+                })
+                .PrimaryKey(t => new { t.UserId, t.RoleId })
+                .ForeignKey("dbo.AspNetRoles", t => t.RoleId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId)
+                .Index(t => t.RoleId);
+
+            CreateTable(
+                "dbo.AspNetUsers",
+                c => new
+                {
+                    Id = c.String(nullable: false, maxLength: 128),
+                    Email = c.String(maxLength: 256),
+                    EmailConfirmed = c.Boolean(nullable: false),
+                    PasswordHash = c.String(),
+                    SecurityStamp = c.String(),
+                    PhoneNumber = c.String(),
+                    PhoneNumberConfirmed = c.Boolean(nullable: false),
+                    TwoFactorEnabled = c.Boolean(nullable: false),
+                    LockoutEndDateUtc = c.DateTime(),
+                    LockoutEnabled = c.Boolean(nullable: false),
+                    AccessFailedCount = c.Int(nullable: false),
+                    UserName = c.String(nullable: false, maxLength: 256),
+                })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.UserName, unique: true, name: "UserNameIndex");
+
+            CreateTable(
+                "dbo.AspNetUserClaims",
+                c => new
+                {
+                    Id = c.Int(nullable: false, identity: true),
+                    UserId = c.String(nullable: false, maxLength: 128),
+                    ClaimType = c.String(),
+                    ClaimValue = c.String(),
+                })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+
+            CreateTable(
+                "dbo.AspNetUserLogins",
+                c => new
+                {
+                    LoginProvider = c.String(nullable: false, maxLength: 128),
+                    ProviderKey = c.String(nullable: false, maxLength: 128),
+                    UserId = c.String(nullable: false, maxLength: 128),
+                })
+                .PrimaryKey(t => new { t.LoginProvider, t.ProviderKey, t.UserId })
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+
+            CreateTable(
+                "dbo.ProductOrder",
+                c => new
+                {
+                    ProductId = c.Int(nullable: false),
+                    OrderDetailId = c.Int(nullable: false),
+                })
+                .PrimaryKey(t => new { t.ProductId, t.OrderDetailId })
+                .ForeignKey("dbo.Products", t => t.ProductId, cascadeDelete: true)
+                .ForeignKey("dbo.OrderDetails", t => t.OrderDetailId, cascadeDelete: true)
+                .Index(t => t.ProductId)
+                .Index(t => t.OrderDetailId);
+
+        }
+
+        public override void Down()
+        {
+            DropForeignKey("dbo.AspNetUserRoles", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserLogins", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserClaims", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserRoles", "RoleId", "dbo.AspNetRoles");
+            DropForeignKey("dbo.OrderDetails", "SalesmanId", "dbo.Salesmen");
+            DropForeignKey("dbo.ProductOrder", "OrderDetailId", "dbo.OrderDetails");
+            DropForeignKey("dbo.ProductOrder", "ProductId", "dbo.Products");
+            DropForeignKey("dbo.Products", "CategoryId", "dbo.Categories");
+            DropForeignKey("dbo.OrderDetails", "BillTypeId", "dbo.BillTypes");
+            DropIndex("dbo.ProductOrder", new[] { "OrderDetailId" });
+            DropIndex("dbo.ProductOrder", new[] { "ProductId" });
+            DropIndex("dbo.AspNetUserLogins", new[] { "UserId" });
+            DropIndex("dbo.AspNetUserClaims", new[] { "UserId" });
+            DropIndex("dbo.AspNetUsers", "UserNameIndex");
+            DropIndex("dbo.AspNetUserRoles", new[] { "RoleId" });
+            DropIndex("dbo.AspNetUserRoles", new[] { "UserId" });
+            DropIndex("dbo.AspNetRoles", "RoleNameIndex");
+            DropIndex("dbo.Products", new[] { "CategoryId" });
+            DropIndex("dbo.OrderDetails", new[] { "SalesmanId" });
+            DropIndex("dbo.OrderDetails", new[] { "BillTypeId" });
+            DropTable("dbo.ProductOrder");
+            DropTable("dbo.AspNetUserLogins");
+            DropTable("dbo.AspNetUserClaims");
+            DropTable("dbo.AspNetUsers");
+            DropTable("dbo.AspNetUserRoles");
+            DropTable("dbo.AspNetRoles");
+            DropTable("dbo.Salesmen");
+            DropTable("dbo.Categories");
+            DropTable("dbo.Products");
+            DropTable("dbo.OrderDetails");
+            DropTable("dbo.BillTypes");
+        }
+    }
+}
--- src/ProStoSystem.Database/Migrations/Configuration.cs
+++ src/ProStoSystem.Database/Migrations/Configuration.cs
@@ -1,9 +1,11 @@
 namespace ProStoSystem.Database.Migrations
 {
-    using System;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using Entities;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Shared;
 
     internal sealed class Configuration : DbMigrationsConfiguration<ProStoSystem.Database.ApplicationDbContext>
     {
@@ -14,18 +16,64 @@ namespace ProStoSystem.Database.Migrations
 
         protected override void Seed(ProStoSystem.Database.ApplicationDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            SeedRoles(context);
+            SeedBillTypes(context);
+            SeedOwnerAccount(context);
+        }
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+        private void SeedRoles(ApplicationDbContext context)
+        {
+            var roleStore = new RoleStore<IdentityRole>(context);
+            var roleManager = new RoleManager<IdentityRole>(roleStore);
+
+            if (!roleManager.RoleExists(AppStrings.OwnerRoleName))
+            {
+                var role = new IdentityRole { Name = AppStrings.OwnerRoleName };
+                roleManager.Create(role);
+            }
+        }
+
+        // I know it's not the best solution. It's temporary.
+        // If you get here and you may have some ideas for creating
+        // owner account from app without registration, send me message :)
+        private void SeedOwnerAccount(ApplicationDbContext context)
+        {
+            var userStore = new UserStore<ApplicationUser>(context);
+            var userManager = new UserManager<ApplicationUser>(userStore);
+
+            var ownerAccount = new ApplicationUser();
+            ownerAccount.UserName = "owner.pssystem@gmail.com";
+            ownerAccount.Email = "owner.pssystem@gmail.com";
+            ownerAccount.EmailConfirmed = true;
+
+            if (!context.Users.Any(u => u.UserName == ownerAccount.UserName))
+            {
+                var ownerResult = userManager.Create(ownerAccount, "Zxc!23");
+
+                if (ownerResult.Succeeded)
+                {
+                    userManager.AddToRole(ownerAccount.Id, AppStrings.OwnerRoleName);
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        private void SeedBillTypes(ApplicationDbContext context)
+        {
+            var receipt = new BillType { Name = "Receipt" };
+            var invoice = new BillType { Name = "Invoice" };
+
+            if (!context.BillTypes.Any(bt => bt.Name == receipt.Name))
+            {
+                context.Set<BillType>().AddOrUpdate(receipt);
+                context.SaveChanges();
+            }
+
+            if (!context.BillTypes.Any(bt => bt.Name == invoice.Name))
+            {
+                context.Set<BillType>().AddOrUpdate(invoice);
+                context.SaveChanges();
+            }
         }
     }
 }
